3059fb46e6e552aeff4ef84b7f2e00ee92d80d5d
Diego Rocha <dlmrocha87@gmail.com>
2021-04-29 11:43:50 -0300

    feat: add head meta tag

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,13 @@
       <Title>
         Quem sou
       </Title>
-      <p class="px-8 font-redhat tracking-wide">
+      <p class="px-8 tracking-wide font-redhat">
       Sou <Highlight>especialista</Highlight> no ensino de línguas estrangeiras
       com mais de 10 anos de <Highlight>experiência</Highlight>, pós-graduada
       em inglês e nativa em espanhol.
       </p>
 
-      <p class="px-8 font-redhat tracking-wide mt-4">
+      <p class="px-8 mt-4 tracking-wide font-redhat">
       Ensino <Highlight>espanhol e inglês</Highlight> a pessoas que sabem que
       falar um idioma estrangeiro nos dias de hoje traz muitas vantagens a
       nível pessoal e profissional. Ajudando meus alunos a ganharem
@@ -32,17 +32,17 @@
       <Title class="mt-12">
         Aprenda com Basi
       </Title>
-      <p class="px-8 font-redhat tracking-wide">
+      <p class="px-8 tracking-wide font-redhat">
       Atualmente tenho trabalhado com <Highlight>aulas online</Highlight> VIP
       ou em grupo, e tenho certeza que eu tenho as qualidades necessárias para
       te ajudar a dar o <Highlight>próximo</Highlight> <Highlight>passo</Highlight> no desenvolvimento
       e na confiança do seu inglês ou espanhol!!
       </p>
 
-      <p class="px-8 font-redhat tracking-wide mt-4">
+      <p class="px-8 mt-4 tracking-wide font-redhat">
       Vem comigo na aventura de se <Highlight>comunicar como você quer</Highlight>!
       </p>
-      <Button class="mt-8 mx-auto" @click="gotoEuyome()">estude comigo</Button>
+      <Button class="mx-auto mt-8" @click="gotoEuyome()">estude comigo</Button>
       </ContainerRow>
     </section>
 
@@ -80,7 +80,66 @@ export default Vue.extend({
     gotoEuyome() {
       window.open('https://euyo.me/speakniceidiomas/6089dd252dd4910e72774c63', '_blank');
     }
-  }
+  },
+
+  head() {
+    const title = 'Idiomas com Basi';
+    const description = 'Olá, eu sou a Basilia especialista em Inglês e Espanhol';
+
+    return {
+      title,
+
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: description,
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://idiomascombasi.com.br',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: description,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content:
+            'https://res.cloudinary.com/euyome/image/upload/v1619704120/idiomascombasi/basi_avatar_hfx2df.png',
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: 'Basilia Santiago',
+        },
+        {
+          hid: 'og:image:width',
+          property: 'og:image:width',
+          content: '500',
+        },
+        {
+          hid: 'og:image:height',
+          property: 'og:image:height',
+          content: '500',
+        },
+      ],
+    };
+  },
 })
 </script>
 
