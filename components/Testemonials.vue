423b4e02a8c910175ce7e93e68beb84e7aaf102f
Diego Rocha <dlmrocha87@gmail.com>
2021-05-05 07:45:32 -0300

    fix testemonials pic size and add fernando prado

--- components/Testemonials.vue
+++ components/Testemonials.vue
@@ -2,10 +2,10 @@
   <div class="mx-auto md:max-w-3/4">
     <Title>O que falam de mim</Title>
     <ul ref="accordionContainer" class="px-8 md:p-0 md:mt-12">
-      <li v-for="t in testemonials" :key="t.id" class="my-4">
+      <li v-for="t in testemonials" :key="t.id" class="my-4 md:my-2">
         <a v-if="!t.visible" class="cursor-pointer focus:outline-none" @click="toggleVisibility(t)">
           <div class="font-medium text-left text-gray-500 font-redhat">
-            <span class="mr-2 text-gray-400 md:hidden">+</span>
+            <span class="mr-2 text-gold md:hidden">+</span>
             <span id="author-name">{{ t.name }}</span> - <span class="text-xs font-normal">{{ t.title }}</span>
           </div>
         </a>
@@ -15,15 +15,30 @@
 
         <div :ref="'accordion-item-'+t.id" class="flex flex-wrap ml-2 overflow-hidden md:ml-4 transition-all duration-300 ease-in-out transform md:flex-nowrap"
                                            :class="{ 'my-4': t.visible }"
-                                           :style="t.visible ? 'height: '+height+'px' : 'height: 0'">
-          <div class="p-4 font-normal tracking-wide text-gray-500 border-l md:border-b border-gold md:pr-24 md:w-3/4 font-redhat" v-html="t.content"> </div>
-          <div class="w-full mt-4 mb-4 text-center md:mt-0 md:max-h-full md:w-1/4 md:mb-0">
-            <img  class="object-contain w-full md:object-center md:h-72 md:object-cover"
+                                           :style="t.visible ? heightStyle : 'height: 0'">
+          <div class="border-l md:border-b border-gold md:pr-24 md:w-3/4">
+            <div class="p-4 font-normal tracking-wide text-gray-500 font-redhat" v-html="t.content"></div>
+            <!-- <Button class="m-4" small>Website</Button> -->
+            <!-- <div class="flex items-center justify-center w-24 h-8 m-4 text-xs text-white bg-black">Website</div> -->
+          </div>
+          <div class="hidden w-1/4 mt-0 mb-0 text-center md:block" :style="heightStyle">
+            <img  class="object-cover object-center" :style="heightStyle"
                   :src="t.image"
                   alt="" >
           </div>
+            <!-- show a fixed size image on small screen -->
+          <div class="w-full h-40 mt-2 mb-4 text-center md:hidden">
+            <img  class="object-contain h-40" :src="t.image" alt="" >
+          </div>
         </div>
       </li>
+
+      <!-- <li class="mt-8 mb-8 md:mb-2"> -->
+      <!--   <div class="text-sm font-medium text-left text-gray-500 font-redhat"> -->
+      <!--       <span class="mr-2 text-gold">∞</span> -->
+      <!--       <span id="author-name">E muito mais</span> -->
+      <!--     </div> -->
+      <!-- </li> -->
     </ul>
   </div>
 </template>
@@ -33,7 +48,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      height: 300,
+      height: 320,
       testemonials: [
         {
           id: 1,
@@ -67,8 +82,27 @@ export default Vue.extend({
           image: 'https://res.cloudinary.com/euyome/image/upload/v1620139898/idiomascombasi/laryssa_cordeiro_xppwyx.jpg',
           visible: false,
         },
+        {
+          id: 5,
+          name: 'Fernando Prado',
+          title: 'Locutor Trilíngue',
+          content: 'Trabalhei com a Basi em alguns projetos comerciais e institucionais, de abrangência nacional e gostei muito do resultado. <spam class="text-black">Tive rapidez e qualidade em todas as etapas do projeto</spam>. Trabalharemos mais em breve.',
+          image: 'https://res.cloudinary.com/euyome/image/upload/v1620209210/idiomascombasi/fernando_prado_ucp3xx.jpg',
+          visible: false,
+        },
       ],
     }
+  },
+
+  computed: {
+    heightStyle(): string {
+      return `height: ${this.height}px;`
+    }
+  },
+
+  mounted() {
+      let item: any = this.$refs['accordion-item-1'];
+      this.height = item[0].scrollHeight;
   },
 
   methods: {
