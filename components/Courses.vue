70eb0ae4ca305572aab7712d5c09a4f66b437267
Diego Rocha <dlmrocha87@gmail.com>
2021-05-09 17:04:50 -0300

    add translations

--- components/Courses.vue
+++ components/Courses.vue
@@ -1,41 +1,110 @@
+<i18n lang="yaml">
+en:
+  titleOne: Learn with Basi
+  subtitleOne: Yes, I can
+  buttonOne: Study with me
+  contentOne: >-
+    Currently, I have worked with online private or group classes and I am
+    confident that I have the right qualities to help you take the next step in
+    developing your English or Spanish confidence.
+
+
+  titleTwo: Private or Pairs classes
+  subtitleTwo: You and me
+  buttonTwo: Schedule your class
+  contentTwo: >-
+    Private or Pairs classes in English or Spanish, with a duration of one hour
+    through the Zoom platform. Flexible schedules and with the material on my
+    own, you will study the desired language at your own pace and with current
+    topics, schedule your free sample class. 
+
+  titleThree: Group classes
+  subtitleThree: You and us
+  buttonThree: Schedule your class
+  contentThree: >-
+    English or Spanish classes, small groups of classes through Zoom, share
+    your class time with other people who have the same goal as you. Check
+    available schedules and take your first class for free.
+
+
+pt-BR:
+  titleOne: Aprenda com Basi
+  subtitleOne: Sim, eu posso!
+  buttonOne: estude comigo
+  contentOne: >-
+    Atualmente tenho trabalhado com aulas online VIP ou em grupo, e tenho
+    certeza que eu tenho as qualidades necessárias para te ajudar a dar o
+    próximo passo no desenvolvimento e na confiança do seu inglês ou espanhol!!
+
+
+  titleTwo: Aulas VIP ou em Dupla
+  subtitleTwo: Você e eu!
+  buttonTwo: agende sua aula
+  contentTwo: >-
+    Aulas VIP ou em Dupla, em inglês ou espanhol, com duração de uma hora pela
+    plataforma ZOOM.  Horários flexivéis e com o material por minha conta, você
+    vai estudar o idioma desejado no seu ritmo e com assuntos atuais, agende
+    sua aula experimental gratuita.
+
+  titleThree: Aulas em Grupo
+  subtitleThree: Você e nós!
+  buttonThree: agende sua aula
+  contentThree: >-
+    Aulas em inglês ou espanhol, em turmas reduzidas pela plataforma
+    ZOOM, compartilhe sua hora de aula com outras pessoas que tem o mesmo
+    objetivo que você!!! Verifique os horários de turmas disponíveis e faça sua
+    primeira aula grátis!
+
+es:
+  titleOne: Aprende con Basi
+  subtitleOne: ¡Sí, yo puedo!
+  buttonOne: Estudia conmigo
+  contentOne: >-
+    Actualmente, he trabajado con clases en línea individuales o en grupos y
+    tengo la certeza que tengo las cualidades necesarias para ayudarte a dar el
+    próximo paso en el desarrollo de la confianza de tu Inglés o Español.
+
+  titleTwo: Clases individuales o en parejas
+  subtitleTwo: Tú y yo
+  buttonTwo: Agenda tu clase
+  contentTwo: >-
+    Clases Individuales o en Parejas en inglés o español, con duración de una
+    hora por la plataforma Zoom. Horarios flexibles y con el material por mi
+    cuenta, vas a estudiar el idioma deseado a tu ritmo y con temas actuales,
+    agenda tu clase de muestra gratuita.
+
+  titleThree: Clases en grupo
+  subtitleThree: Tú y nosotros
+  buttonThree: Agenda tu clase
+  contentThree: >-
+    Clases en inglés o en español, en grupos de clases reducidos por la
+    plataforma Zoom, comparta su hora de clase con otras personas que tienen el
+    mismo objetivo que tú. Consulte los horarios disponibles y haga su primera
+    clase gratis.
+</i18n>
+
 <template>
   <div>
-    <ContentSection title="Aprenda com Basi"
-                    subtitle="Sim, eu posso!"
-                    button-title="estude comigo" url="https://euyo.me/idiomascombasi"
+    <ContentSection :title="$t('titleOne')"
+                    :subtitle="$t('subtitleOne')"
+                    :button-title="$t('buttonOne')" url="https://euyo.me/idiomascombasi"
                     left>
-    <Paragraph>
-    Atualmente tenho trabalhado com <Highlight>aulas online</Highlight> VIP
-    ou em grupo, e tenho certeza que eu tenho as qualidades necessárias para
-    te ajudar a dar o <Highlight>próximo</Highlight> <Highlight>passo</Highlight> no desenvolvimento
-    e na confiança do seu inglês ou espanhol!!
-    </Paragraph>
+    <Paragraph>{{ $t('contentOne') }}</Paragraph>
     </ContentSection>
 
-    <ContentSection title="Aulas VIP ou em Dupla"
-                    subtitle="Você e eu!"
-                    buttonTitle="agende sua aula"
+    <ContentSection :title="$t('titleTwo')"
+                    :subtitle="$t('subtitleTwo')"
+                    :buttonTitle="$t('buttonTwo')"
                     url="https://euyo.me/idiomascombasi">
-    <Paragraph>
-        Aulas VIP ou em Dupla, em inglês ou espanhol, com duração de uma hora
-        pela plataforma ZOOM.  <Highlight>Horários flexivéis</Highlight> e com
-        o material por minha conta, você vai estudar o idioma desejado no
-        <Highlight>seu ritmo</Highlight> e com assuntos atuais, agende sua aula
-        <Highlight>experimental gratuita</Highlight>.
-    </Paragraph>
+    <Paragraph>{{ $t('contentTwo') }}</Paragraph>
     </ContentSection>
 
-    <ContentSection title="Aulas em Grupo"
-                    subtitle="Você e nós!"
-                    buttonTitle="agende sua aula"
+    <ContentSection :title="$t('titleThree')"
+                    :subtitle="$t('subtitleThree')"
+                    :buttonTitle="$t('buttonThree')"
                     url="https://euyo.me/idiomascombasi"
                     left>
-    <Paragraph>
-          Aulas em inglês ou espanhol, em <Highlight>turmas reduzidas</Highlight> pela plataforma
-          ZOOM, compartilhe sua hora de aula com outras pessoas que tem o mesmo
-          objetivo que você!!! Verifique os horários de turmas disponíveis e faça sua
-          <Highlight>primeira aula grátis</Highlight>!
-    </Paragraph>
+    <Paragraph>{{ $t('contentThree') }}</Paragraph>
     </ContentSection>
   </div>
 </template>
---
+++ components/Translations.vue
@@ -0,0 +1,39 @@
+<i18n lang="yaml">
+en:
+  title: Translations
+  subtitle: Expand your target audience
+  button: Make a free quote
+
+  content: >-
+    Professional translations in English and Spanish. Make your content reach
+    more people, boosting your results. Make a free quote, contact me by
+    clicking on the button below! 
+
+pt-BR:
+  title: Traduções
+  subtitle: Amplie seu público-alvo
+  button: Faça seu Orçamento
+
+  content: >-
+    Traduções profisionais em inglês e espanhol. Faça seu conteúdo chegar a
+    mais pessoas, potencializando os seus resultados! Faça um orçamento sem
+    compromisso entrando em contato comigo pelo botão abaixo!
+
+es:
+  title: Traducciones
+  subtitle: Amplíe su público alvo
+  button: cotiza aquí
+
+  content: >-
+    Traducciones profesionales en Inglés y Español. Haga su contenido llegar a
+    más personas, potencializando sus resultados. ¡Haga una cotización sin
+    compromiso, entra en contacto conmigo clicando en el botón de abajo! 
+</i18n>
+
+<template>
+  <ContentSection :title="$t('title')" :subtitle="$t('subtitle')" :button-title="$t('button')" url="https://euyo.me/idiomascombasi" >
+  <Paragraph>
+  {{ $t('content') }}
+  </Paragraph>
+  </ContentSection>
+</template>
