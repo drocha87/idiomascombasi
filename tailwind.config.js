2f05ad0a75d7f1f8650403c20f0ebab47565f564
Diego Rocha <dlmrocha87@gmail.com>
2021-06-08 07:17:11 -0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,48 @@
-export const purge = []
-export const darkMode = false
+module.exports = {
+  purge: [],
+  darkMode: false,
 
-export const theme = {
-  extend: {
-    fontFamily: {
-      antonio: ['Antonio', 'sans-serif'],
-      dancing: ['Dancing Script', 'cursive'],
-      redhat: ['Red Hat Display', 'sans-serif'],
-      raleway: ['Raleway', 'sans-serif'],
-      montserrat: ['Montserrat', 'sans-serif'],
-    },
+  theme: {
+    extend: {
+      fontFamily: {
+        antonio: ['Antonio', 'sans-serif'],
+        dancing: ['Dancing Script', 'cursive'],
+        redhat: ['Red Hat Display', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
 
-    backgroundImage: {
-      'hero-image':
-        "url('https://res.cloudinary.com/euyome/image/upload/v1620067471/idiomascombasi/basi_hero_wider_ehkspo.jpg')",
-    },
+      backgroundImage: {
+        'hero-image':
+          "url('https://res.cloudinary.com/euyome/image/upload/v1620067471/idiomascombasi/basi_hero_wider_ehkspo.jpg')",
+      },
 
-    maxWidth: {
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-    },
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
 
-    minWidth: {
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-    },
-    colors: {
-      primary: '#A7DADC',
-      secondary: '#120e41',
-      gold: '#c6a869',
-      fafa: '#fafafa',
-    },
+      minWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
+      colors: {
+        primary: '#A7DADC',
+        secondary: '#120e41',
+        gold: '#c6a869',
+        fafa: '#fafafa',
+      },
 
-    transitionProperty: {
-      'max-height': 'max-height',
+      transitionProperty: {
+        'max-height': 'max-height',
+      },
     },
   },
-}
 
-export const variants = {
-  extend: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
-export const plugins = []
